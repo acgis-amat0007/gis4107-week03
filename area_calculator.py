e0a9db9214435cfb1722774cdbbadb95023131c1
Marie-SoleilK <king0419@algonquinlive.com>
2023-09-19 14:57:23 -0400

    AA

--- area_calculator.py
+++ area_calculator.py
@@ -5,10 +5,10 @@ def get_circle_area(radius):
     """Returns the area of a circle of the given radius
     """
     circle_area=pi*(radius**2)
-    print(f'{circle_area} m2')
+    #print(f'{circle_area} m2')
 
 def get_square_area(side):
     """Returns the area of a square of the given side length"""
     square_area=side**2
-    print(square_area)
+    #print(square_area)
     
