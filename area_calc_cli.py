ee5a8f58203ddfc0c693841113c6ed50d546cf3d
acgis-amat0007 <amat0007@algonquinlive.com>
2023-09-19 15:15:52 -0400

    AA

--- area_calc_cli.py
+++ area_calc_cli.py
@@ -1,3 +1,4 @@
 import area_calculator
 
-area_calculator.get_circle_area(2)
+print(area_calculator.get_circle_area(2))
+print(area_calculator.get_square_area(2))
--- area_calculator.py
+++ area_calculator.py
@@ -4,10 +4,10 @@ from math import pi
 def get_circle_area(radius):
     """Returns the area of a circle of the given radius
     """
-    print(pi*(radius**2))
+    return(pi*(radius**2))
 
 def get_square_area(side):
     """Returns the area of a square of the given side length"""
     
-    print(side**2)
+    return(side**2)
     
