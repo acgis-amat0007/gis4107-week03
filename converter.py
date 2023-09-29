f1d7c9fd01294e8b4f22e35bf9bf5167d174e498
acgis-amat0007 <amat0007@algonquinlive.com>
2023-09-29 09:12:43 -0400

    AA

--- converter.py
+++ converter.py
@@ -1,7 +1,7 @@
 """Creates functions from assignment 1 exercises"""
 
 def miles_to_km(miles):
-    """Converts miles to kilomeeters"""
+    """Converts miles to kilometers"""
     MI_TO_KM = 1.609
     kilometers = MI_TO_KM * miles
     return miles, kilometers
@@ -24,3 +24,18 @@ def sqmetres_to_acres(sqm_4):
     ac = sqm_4/SQM_TO_AC
     return sqm_4, ac
 
+def acres_to_edge_of_square():
+    """Calculates edge length of a square acres in meters"""
+
+
+def get_bear_count():
+    """Calculates estimated number of bears of sample density in a given area"""
+
+def dms_to_dd():
+    """Converts Degrees:Minutes:Seconds to Decimel Degrees"""
+
+def dd_to_dms():
+    """Converts Decimel Degrees to Degrees:Minutes:Seconds"""
+
+def get_fuel_cost():
+    """Calculates fuel cost for a given distance at a given milage and fuel cost"""
